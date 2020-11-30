b38254fe34505f5c2319ab9e3c70182a5728f916
Ankit Tripathi <ankittripathi@InnovationM-Admins-MacBook-Pro.local>
2020-11-30 22:42:50 +0530

    AOP basics also added to code

---
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,32 @@
+package com.luv2code.springdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forDaoPackage() || forServicePackage() || forControllerPackage()")
+	private void forAppFlow() {}
+	
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJP) {
+		System.out.println("@Before : calling method is : " + theJP.getSignature().toShortString());
+	}
+	
+}
